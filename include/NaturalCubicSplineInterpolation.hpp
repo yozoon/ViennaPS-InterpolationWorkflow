4a83ddc990faaac2ccdb0e7e80617621b7a03b93
Julius Piso <julius@piso.at>
2023-01-17 19:36:55 +0100

    Spline class initialization deferred to first invocation of call operator.

--- include/NaturalCubicSplineInterpolation.hpp
+++ include/NaturalCubicSplineInterpolation.hpp
@@ -10,11 +10,35 @@ class NaturalCubicSplineInterpolation {
   using SizeType = size_t;
 
   static constexpr size_t outputDimension = std::tuple_size_v<OutputType>;
+  bool initialized = false;
 
 public:
   NaturalCubicSplineInterpolation(const KnotContainerType &passedKnots,
                                   const ValueContainerType &passedF)
-      : knots(passedKnots), f(passedF), N(knots.size()) {
+      : knots(passedKnots), f(passedF), N(knots.size()) {}
+
+  OutputType operator()(NumericType x) {
+    if (!initialized)
+      initialize();
+
+    auto lowerBound = sorted.lower_bound(x);
+
+    SizeType i = 1;
+    SizeType d = std::distance(sorted.begin(), lowerBound);
+    i = std::clamp(d, SizeType{1}, SizeType{N - 1});
+
+    NumericType t = (x - knots.at(i - 1)) / (knots.at(i) - knots.at(i - 1));
+
+    typename ValueContainerType::value_type result;
+    for (SizeType j = 0; j < outputDimension; ++j) {
+      result[j] = (1. - t) * f.at(i - 1).at(j) + t * f.at(i).at(j) +
+                  t * (1. - t) * ((1. - t) * a(i, j) + t * b(i, j));
+    }
+    return result;
+  }
+
+private:
+  void initialize() {
     Eigen::Matrix<NumericType, Eigen::Dynamic, Eigen::Dynamic> A;
     A.resize(N, N);
 
@@ -83,26 +107,10 @@ public:
     // Now create a set based on the knots, so that we can easily look up the
     // range in which the point that is to be interpolated is located.
     sorted = std::set<NumericType>(knots.begin(), knots.end());
+
+    initialized = true;
   }
 
-  OutputType operator()(NumericType x) const {
-    auto lowerBound = sorted.lower_bound(x);
-
-    SizeType i = 1;
-    SizeType d = std::distance(sorted.begin(), lowerBound);
-    i = std::clamp(d, SizeType{1}, SizeType{N - 1});
-
-    NumericType t = (x - knots.at(i - 1)) / (knots.at(i) - knots.at(i - 1));
-
-    typename ValueContainerType::value_type result;
-    for (SizeType j = 0; j < outputDimension; ++j) {
-      result[j] = (1. - t) * f.at(i - 1).at(j) + t * f.at(i).at(j) +
-                  t * (1. - t) * ((1. - t) * a(i, j) + t * b(i, j));
-    }
-    return result;
-  }
-
-private:
   const KnotContainerType knots;
   const ValueContainerType f;
   const SizeType N;
